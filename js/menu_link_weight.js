1ab98d0e0d1aac69a8d7e18f1b595d095b11afdb
indigoxela <user@olga.indigofloat.at>
2020-07-31 17:12:42 +0200

    Trigger also on checkbox change, Fixes #1

--- js/menu_link_weight.js
+++ js/menu_link_weight.js
@@ -15,16 +15,26 @@
         var $current_selection = $('.menu-link-weight-link-current', context);
         var $node_title = $(this).closest('form').find('.form-item-title input');
         // If there is no title, take over the title of the link.
-        if ($current_selection.html() == '') {
+        if ($current_selection.html() === '') {
           $current_selection.html($link_title.val().substring(0, 30));
         }
         // Take over any link title change.
-        $link_title.keyup(function () {
+        $link_title.on('keyup', function () {
           $current_selection.html($link_title.val().substring(0, 30));
         });
         // Also update on node title change, as this may update the link title.
-        $node_title.keyup(function () {
-          $current_selection.html($link_title.val().substring(0, 30));
+        $node_title.on('keyup', function () {
+          if ($checkbox.is(':checked') && !$link_title.data('menuLinkAutomaticTitleOverridden')) {
+            $current_selection.html($link_title.val().substring(0, 30));
+          }
+        });
+        // Checking and unchecking the checkbox may also update the link title.
+        $checkbox.on('change', function() {
+          if ($checkbox.is(':checked') && !$link_title.data('menuLinkAutomaticTitleOverridden')) {
+            if ($node_title.val() !== undefined) {
+              $current_selection.html($node_title.val().substring(0, 30));
+            }
+          }
         });
       });
     }
